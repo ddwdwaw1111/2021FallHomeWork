58e37e706103cc5a71a451884061e39529a8e39f
ddwdwaw1111 <55290301+ddwdwaw1111@users.noreply.github.com>
2021-10-03 23:27:31 -0400

    all pages for fitness tracker

--- server/index.js
+++ server/index.js
@@ -3,13 +3,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app
-    .get('/', (req, res) => {
-        res.send('Hello World!')
-    })
-    .get('/newpaltz', (req, res) => {
-        res.send('Hello Newpaltz!')
-    })
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
